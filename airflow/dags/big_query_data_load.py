9289bafd13a5c8339cae060c40a4f6e22319a6f0
juliocnsouza <juliocnsouzadev@gmail.com>
2021-02-18 09:14:02 -0300

    add latest data task

--- airflow/dags/big_query_data_load.py
+++ airflow/dags/big_query_data_load.py
@@ -12,6 +12,7 @@ from airflow.contrib.hooks.gcp_pubsub_hook import PubSubHook
 
 
 from airflow.contrib.operators.gcs_to_bq import GoogleCloudStorageToBigQueryOperator
+from airflow.contrib.operators.bigquery_operator import BigQueryOperator
 
 # settings
 DAG_NAME = "LOAD-DATA-INTO-BIGQUERY"
@@ -22,10 +23,26 @@ EMAILS = ["juliocnsouzadev@gmail.com"]
 PROJECT_ID = "muvirtua"
 BIG_QUERY_DATASET = "vehicle_analysis"
 LANING_BUCKET = "01-logistics-landing"
-
+BQ_HISTORY_TABLE = PROJECT_ID + "." + BIG_QUERY_DATASET + ".history"
+BQ_LATEST_TABLE = PROJECT_ID + "." + BIG_QUERY_DATASET + ".latest"
 
 # tasks ids
 T1_ID = "T1-LOAD_DATA"
+T2_ID = "T1-FILTER_LATEST"
+
+LATEST_QUERY = """
+SELECT * except (rank)
+FROM (
+    SELECT
+    *,
+    ROW_NUMBER() OVER (
+        PARTITION BY vehicle_id ORDER BY DATETIME(date, TIME(hour, minute, 0)) DESC
+    ) as rank
+    FROM `{}`) as latest
+WHERE rank = 1;
+""".format(
+    BQ_HISTORY_TABLE
+)
 
 default_arguments = {
     "owner": OWNER_NAME,
@@ -54,12 +71,22 @@ with DAG(
         source_objects=["*"],
         skip_leading_rows=1,
         field_delimiter=",",
-        destination_project_dataset_table=PROJECT_ID
-        + "."
-        + BIG_QUERY_DATASET
-        + ".history",
-        create_disposition="CREATE_IF_NEEEDED",
+        destination_project_dataset_table=BQ_HISTORY_TABLE,
+        create_disposition="CREATE_IF_NEEDED",
         write_disposition="WRITE_APPEND",
         bigquery_conn_id="google_cloud_default",
         google_cloud_storage_conn_id="google_cloud_default",
     )
+
+    t2 = BigQueryOperator(
+        task_id=T2_ID,
+        sql=LATEST_QUERY,
+        destination_dataset_table=BQ_LATEST_TABLE,
+        write_disposition="WRITE_TRUNCATE",
+        create_disposition="CREATE_IF_NEEDED",
+        use_legacy_sql=False,
+        location="US",
+        bigquery_conn_id="google_cloud_default",
+    )
+
+t1 >> t2
