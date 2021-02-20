21123d7165702f2b9e4b8bd42c158f07ef50209e
juliocnsouza <juliocnsouzadev@gmail.com>
2021-02-20 06:12:20 -0300

    update bucket

--- airflow/pyspark/weekday/avg_speed.py
+++ airflow/pyspark/weekday/avg_speed.py
@@ -1,27 +1,26 @@
 #!/usr/bin/python
 from pyspark.sql import SparkSession
 
-spark = SparkSession \
-  .builder \
-  .master('yarn') \
-  .appName('bigquery-analytics-avg-speed') \
-  .getOrCreate()
+spark = (
+    SparkSession.builder.master("yarn")
+    .appName("bigquery-analytics-avg-speed")
+    .getOrCreate()
+)
 
-bucket = 'YOUR-BUCKET-NAME'
-spark.conf.set('temporaryGcsBucket', bucket)
+bucket = "01-logistics-backup"
+spark.conf.set("temporaryGcsBucket", bucket)
 
-history = spark.read.format('bigquery') \
-  .option('table', 'vehicle_analytics.history') \
-  .load()
-history.createOrReplaceTempView('history')
+history = (
+    spark.read.format("bigquery").option("table", "vehicle_analytics.history").load()
+)
+history.createOrReplaceTempView("history")
 
 avg_speed = spark.sql(
-    'SELECT vehicle_id, date, AVG(speed) AS avg_speed FROM history GROUP BY vehicle_id, date'
+    "SELECT vehicle_id, date, AVG(speed) AS avg_speed FROM history GROUP BY vehicle_id, date"
 )
 avg_speed.show()
 avg_speed.printSchema()
 
-avg_speed.write.format('bigquery') \
-    .option('table', 'vehicle_analytics.avg_speed') \
-    .mode('append') \
-    .save()
+avg_speed.write.format("bigquery").option("table", "vehicle_analytics.avg_speed").mode(
+    "append"
+).save()
--- airflow/pyspark/weekday/avg_temperature.py
+++ airflow/pyspark/weekday/avg_temperature.py
@@ -1,27 +1,26 @@
 #!/usr/bin/python
 from pyspark.sql import SparkSession
 
-spark = SparkSession \
-  .builder \
-  .master('yarn') \
-  .appName('bigquery-analytics-avg-temperature') \
-  .getOrCreate()
+spark = (
+    SparkSession.builder.master("yarn")
+    .appName("bigquery-analytics-avg-temperature")
+    .getOrCreate()
+)
 
-bucket = 'YOUR-BUCKET-NAME'
-spark.conf.set('temporaryGcsBucket', bucket)
+bucket = "01-logistics-backup"
+spark.conf.set("temporaryGcsBucket", bucket)
 
-history = spark.read.format('bigquery') \
-  .option('table', 'vehicle_analytics.history') \
-  .load()
-history.createOrReplaceTempView('history')
+history = (
+    spark.read.format("bigquery").option("table", "vehicle_analytics.history").load()
+)
+history.createOrReplaceTempView("history")
 
 avg_temperature = spark.sql(
-    'SELECT vehicle_id, date, AVG(temperature) AS avg_temperature FROM history GROUP BY vehicle_id, date'
+    "SELECT vehicle_id, date, AVG(temperature) AS avg_temperature FROM history GROUP BY vehicle_id, date"
 )
 avg_temperature.show()
 avg_temperature.printSchema()
 
-avg_temperature.write.format('bigquery') \
-    .option('table', 'vehicle_analytics.avg_temperature') \
-    .mode('append') \
-    .save()
+avg_temperature.write.format("bigquery").option(
+    "table", "vehicle_analytics.avg_temperature"
+).mode("append").save()
